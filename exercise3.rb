e847c9e10923f8f3da7c96c0fa6f4821e0997e6c
ayhanbasmisirli <ayhan.basmisirli@gmail.com>
2014-03-25 14:14:33 -0400

    Exercise second step

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,12 @@
 puts "What is your name ?"
 name = gets.chomp
 puts "Your name is #{name}"
+puts "How old are you"
+age =gets.chomp.to_s
+puts "Your age is #{age}"
+puts "Where were you born"
+place = gets.chomp
+puts "You were born #{place}"
+
 
 
