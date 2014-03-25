5888d9a9e1b260c979fba2d3056d15e75ec48a4c
ayhanbasmisirli <ayhan.basmisirli@gmail.com>
2014-03-25 14:01:36 -0400

    second exercise

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#if tip amount is a %10 
+# tips
+# ayhan basmisirli
+amount = 55
+tip = 0.10
+puts "you have to pay to tip for $#{amount} is $#{amount*tip}"
+#second q
+#puts "ayhan basmisirli"+34
+puts "ayhan basmisirli "+34.to_s
+puts "result of 45628*7839 is #{45628*7839} "
+puts (true && false) || (false && true) || !(false && false)
